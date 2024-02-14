eba66091d11300944e6ef8a18d73c6ab530e294e
brwngld <bern587@yahoo.com>
2024-02-14 17:23:09 +0000

    Still not working yet

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given Reddit subreddit."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -16,23 +16,8 @@ def top_ten(subreddit):
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    # Check if the response status code indicates success
-    if response.status_code == 200:
-        # Attempt to parse the JSON response
-        try:
-            data = response.json()
-            # Check if the response contains valid data
-            if data and 'data' in data:
-                children = data['data'].get('children', [])
-                # Print the titles of the posts
-                for child in children:
-                    post_data = child.get('data')
-                    title = post_data.get('title')
-                    print(title)
-            else:
-                print("No data found for subreddit:", subreddit)
-        except ValueError:
-            print("Error: Unable to parse JSON response")
-    else:
-        print("Error:", response.status_code)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
